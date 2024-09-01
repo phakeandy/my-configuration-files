9f20eb55abce6e73f0d99953e249c7d814285580
phakeandy <daihan0508@gmail.com>
2024-09-01 23:41:49 +0800

    update

--- sh_conf/bootstrap.sh
+++ sh_conf/bootstrap.sh
@@ -3,16 +3,28 @@
 current_shell="$SHELL"
 
 if [[ "$current_shell" == *"/bash"* ]]; then
-    echo "您正在使用 Bash. 暂不支持."
-    exit
-elif [[ "$current_shell" == *"/zsh"* ]]; then
-    echo "[*] 正在为 Zsh配置."
-    cat <<EOF >> ~/.zshrc
+    echo "[*] 正在为 Bash 配置."
+    cat <<EOF >> ~/.bashrc
+# 加载配置文件 https://github.com/phakeandy/my-configuration-files
 source ~/.local/my-configuration-files/sh_conf/main.sh
 EOF
-    exit
+    return
+elif [[ "$current_shell" == *"/zsh"* ]]; then
+    echo "[*] 正在为 Zsh 配置."
+    cat <<EOF >> ~/.zshrc
+# 加载配置文件 https://github.com/phakeandy/my-configuration-files
+source ~/.local/my-configuration-files/sh_conf/main.sh
+EOF
+    return
 else
     echo "您正在使用的 shell 既不是 Bash 也不是 Zsh. 暂不支持."
-    exit 
+    return 
 fi
 
+function init_tmux () {
+    cp ../tmux.conf ~/.tmux.conf
+    # 安装 tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    # TODO: 安装插件的脚本
+    # ...
+}
